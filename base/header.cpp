f465a2eb81c80fe27639fb9d15b0f2f211be7f49
fblogy <39649744+fblogy@users.noreply.github.com>
2018-09-19 13:56:24 +0800

    update

--- base/header.cpp
+++ base/header.cpp
@@ -1,28 +1,34 @@
 ﻿#include<bits/stdc++.h>
 using namespace std;
-typedef double db;
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
 #define fi first
 #define se second
 #define mp make_pair
 #define pb push_back
-#define pw(x) (1ll << (x))
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(),(x).end()
-#define rep(i,l,r) for(int i=(l);i<(r);++i)
-#define per(i,l,r) for(int i=(r)-1;i>=(l);--i)
-#define dd(x) cout << #x << " = " << (x) << ", "
-#define de(x) cout << #x << " = " << (x) << "\n"
+#define rep(i, a, b) for(int i=(a); i<(b); i++)
+#define per(i, a, b) for(int i=(b)-1; i>=(a); i--)
+#define sz(a) (int)a.size()
+#define de(a) cout << #a << " = " << a << endl
+#define dd(a) cout << #a << " = " << a << " "
+#define all(a) a.begin(), a.end()
+#define pw(x) (1ll<<(x))
 #define endl "\n"
-
-//-----
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+const int P = 1e9 + 7;
+int add(int a, int b) {if((a += b) >= P) a -= P; return a;}
+int sub(int a, int b) {if((a -= b) < 0) a += P; return a;}
+int mul(int a, int b) {return 1ll * a * b % P;}
+int kpow(int a, int b) {int r=1;for(;b;b>>=1,a=mul(a,a)) {if(b&1)r=mul(r,a);}return r;}
+//----
 
 int main() {
+	freopen("a.in","r",stdin);
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(0);
-
-
+	//cout << setiosflags(ios::fixed);
+	//cout << setprecision(2);
 	return 0;
 }
